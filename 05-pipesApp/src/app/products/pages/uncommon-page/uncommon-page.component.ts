abc5c1c046b15eaa89a5922554c688907b5fd5c1
mintybubblegum <noa.trujillo@bosonit.com>
2023-06-20 12:16:19 +0200

    finished exercise no.160

--- 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipesApp/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -18,5 +18,18 @@ export class UncommonPageComponent {
   changeClient():void {
     this.name = 'Miles';
     this.gender = 'male';
-}
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Natalia', 'Miri', 'Cris', 'Gilles', 'Granada', 'Graciela', 'Inma', 'Carmen', 'Geraldine', 'Antea'];
+  public clientsMap = {
+    '=0': "we don't have any clients waiting in the queue.", 
+    '=1': "we have a client waiting in the queue.", 
+    'other': "we have # clients waiting in the queue.", 
+  }
+
+  deleteClient(): void{
+    this.clients.shift();
+  }
+
 }
