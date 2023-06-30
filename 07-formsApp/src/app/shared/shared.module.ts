8da1632f0ccafc798873d4bb71e1d01a25e1dc69
mintybubblegum <noa.trujillo@bosonit.com>
2023-06-30 08:49:04 +0200

    navbar finished

--- 07-formsApp/src/app/shared/shared.module.ts
+++ 07-formsApp/src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideMenuComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule //cuando usamos routerLink, tenemos que importar este módulo en shared para ser usado
+  ],
+  exports: [
+    SideMenuComponent
   ]
 })
 export class SharedModule { }
