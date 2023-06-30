b33c625f08d50ba8c604460944a4ffc205d50d97
mintybubblegum <noa.trujillo@bosonit.com>
2023-06-30 12:14:44 +0200

    establecer y re-establecer valores a formulario reactivo - exercise 234

--- 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+const product = {
+  name: 'Conga',
+  price: 159,
+  inStorage: 6,
+}
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [
   ]
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
   /* public myForm: FormGroup = new FormGroup({
     name: new FormControl(''), //1-valor por defecto 2-validaciones sincronas 3-validaciones asincronas
@@ -22,11 +28,17 @@ export class BasicPageComponent {
 
   constructor( private fb: FormBuilder ){}
 
+  ngOnInit(): void {
+    this.myForm.reset( product );
+  }
+
   onSave():void {
 
     if ( this.myForm.invalid ) return;
 
     console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 })
     
   }
 
