0af1ce515f79b3bad20a1e2244287f9b278d40fa
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-03 08:27:36 +0200

    dynamic forms - exercise 237

--- 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formsApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
+
+  //estructura inicial del formulario
+  public myDynamicForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    favouriteGames: this.fb.array([
+      ['Crash Bandicoot', Validators.required],
+      ['Tekken 3', Validators.required],
+    ])
+  })
+
+  constructor( private fb:FormBuilder ){}
+
+  get favouriteGamesControls(){
+    return this.myDynamicForm.get('favouriteGames') as FormArray;
+  }
+
+  onSubmit():void{
+
+    if ( this.myDynamicForm.invalid ) {
+      this.myDynamicForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myDynamicForm.value);
+    this.myDynamicForm.reset();
+    
+
+  }
 
 }
