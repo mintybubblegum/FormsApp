d4622eb983e8e17f3735f283b8798dbfd7d6235f
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-03 16:44:34 +0200

    email and pattern validations - exercise 249

--- 07-formsApp/src/app/shared/validations/validations.ts
+++ 07-formsApp/src/app/shared/validations/validations.ts
@@ -1,7 +1,11 @@
-import { FormControl } from "@angular/forms";
+import { FormControl, ValidationErrors } from "@angular/forms";
 
+    //revisar que nombre + espacio + apellido
+export const firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+    //revisar que email@gmail.com
+export const emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
-export const cantBeStrider = ( control: FormControl ) => {
+export const cantBeStrider = ( control: FormControl ): ValidationErrors | null => {
 
     const value: string = control.value.trim().toLowerCase();
 
