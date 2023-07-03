44511016065ec16facfdb4206fb8179723df6214
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-03 16:10:50 +0200

    validator functions to be used in more than one place - shared - exercise 248

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validations/validations';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
 
+  public registerForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required, cantBeStrider ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  })
+
+
+  constructor( private fb: FormBuilder ) {}
+
+  isNotValidField( field: string ) {
+    // TODO: obtener validación desde un servicio
+  }
+
+  onSubmit() {
+    this.registerForm.markAllAsTouched();
+  }
 }
