2c1856e60fb6dd7b549980b8af632e69f6980692
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-04 17:28:44 +0200

    email validator - exercise 252

--- 07-formsApp/src/app/shared/validations/email-validation.service.ts
+++ 07-formsApp/src/app/shared/validations/email-validation.service.ts
@@ -8,6 +8,31 @@ export class EmailValidation implements AsyncValidator {
     validate( control: AbstractControl ): Observable<ValidationErrors | null> {
 
         const email = control.value;
+        
+        const httpCallObservable = new Observable<ValidationErrors | null> ((subs) => {
+        
+            console.log({ email });
+
+            if ( email === 'noa.trujillo@bosonit.com' ) {
+                subs.next({ emailTaken: true });
+                subs.complete();
+                //return;
+            }
+            
+            subs.next(null); //si la persona introduce algún email que no sea 'noa.trujillo@bosonit.com' === no existe/no está tomado
+            subs.complete();
+        }).pipe (
+            delay ( 3000 )
+        )
+
+        return httpCallObservable;
+    }
+
+    //
+
+    /* validate( control: AbstractControl ): Observable<ValidationErrors | null> {
+
+        const email = control.value;
         console.log({ email });
         
 
@@ -16,6 +41,6 @@ export class EmailValidation implements AsyncValidator {
         }). pipe(
             delay( 2000 )
             );
-    }
+    } */
     
 }
