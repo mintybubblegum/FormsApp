7533c094734363889f94bcbd1bc232ddef2da9d8
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-04 16:39:08 +0200

    async validators - exercise 251

--- 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
+++ 07-formsApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { ValidationsService } from 'src/app/shared/service/validations.service';
+import { EmailValidation } from 'src/app/shared/validations/email-validation.service';
 
 
 @Component({
@@ -9,17 +11,19 @@ import { ValidationsService } from 'src/app/shared/service/validations.service';
 export class RegisterPageComponent {
 
   public registerForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.pattern(this.validationsService.firstNameAndLastnamePattern) ]],
-    email: ['', [ Validators.required, Validators.pattern(this.validationsService.emailPattern) ]],
+    name: ['', [ Validators.required, Validators.pattern( this.validationsService.firstNameAndLastnamePattern )  ]],
+    // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
+    email: ['', [ Validators.required, Validators.pattern( this.validationsService.emailPattern )], [ this.emailValidation ]],
     username: ['', [ Validators.required, this.validationsService.cantBeStrider ]],
     password: ['', [ Validators.required, Validators.minLength(6) ]],
-    password2: ['', [ Validators.required ]],
+    password2: ['', [ Validators.required ]]
   })
 
 
   constructor( 
     private fb: FormBuilder,
-    private validationsService: ValidationsService
+    private validationsService: ValidationsService,
+    private emailValidation:EmailValidation
     ) {}
 
   //para que se desaparezcan los validationErrors y sólo aparezcan cuando se pulse 'crear'. Se añade en cada span de register-page.component.html
