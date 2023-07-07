ed3bc62875344fca2f124c8ef598cf0dbadd926d
mintybubblegum <noa.trujillo@bosonit.com>
2023-07-07 12:41:08 +0200

    ngOnDestroy - exercise 275

--- 09-angular-lifecycle-hooks/src/app/products/components/price/price.component.ts
+++ 09-angular-lifecycle-hooks/src/app/products/components/price/price.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'products-price',
@@ -8,10 +9,13 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   
   @Input()
   public price:number = 0;
+  public interval$?: Subscription;
 
   ngOnInit(): void {
     console.log('CHILD component: ngOnInit');
     
+    this.interval$ = interval(1000).subscribe( value => console.log(`Tick: ${value}`) );
+
   }
   ngOnChanges(changes: SimpleChanges): void {
     console.log('CHILD component: ngOnChanges');
@@ -20,6 +24,7 @@ export class PriceComponent implements OnInit, OnChanges, OnDestroy {
   }
   ngOnDestroy(): void {
     console.log('CHILD component: ngOnDestroy');
+    this.interval$?.unsubscribe(); //para evitar que nuestro componente se siga ejecutando y relentice nuestro projecto
   }
 
 }
